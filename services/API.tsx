bca1e6694c449be82d00443d256d405fdff0d77a
martingaidoo <martingaido00@gmail.com>
2024-05-11 20:03:37 -0300

    services

---
+++ services/API.tsx
@@ -0,0 +1,18 @@
+// services/api.js
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:3000/api'; // Reemplaza esto con la URL de tu API
+
+//EJEMPLO
+// Función para obtener todos los productos
+//export const getAllProducts = async () => {
+//  try {
+//    const response = await axios.get(`${BASE_URL}/products`);
+//    return response.data;
+//  } catch (error) {
+//    throw new Error('Error al obtener los productos');
+//  }
+//};
+
+
+// Otras funciones para actualizar, eliminar, etc.
